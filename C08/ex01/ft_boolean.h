ec54464f07100ca950c3b6657b72698eb2242d20
maustel <144211843+maustel@users.noreply.github.com>
2023-09-07 13:25:58 +0200

    Add files via upload

---
+++ C08/ex01/ft_boolean.h
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_boolean.h                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: maustel <maustel@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/09/05 11:32:17 by maustel           #+#    #+#             */
+/*   Updated: 2023/09/06 14:09:15 by maustel          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_BOOLEAN_H
+# define FT_BOOLEAN_H
+
+# include <unistd.h>
+
+typedef int	t_bool;
+
+# define EVEN(x) x % 2 == 0
+# define TRUE 1
+# define FALSE 0
+# define SUCCESS 0
+
+# define EVEN_MSG "I have an even number of arguments.\n"
+# define ODD_MSG "I have an odd number of arguments.\n"
+
+#endif 
